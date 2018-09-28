0239f691a7c367f52ee940d9cc802235e730b5fd
Joseph Boyle <boyle.p.joseph@gmail.com>
2018-09-27 22:38:33 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import {EatonThemes} from '@pxblue/themes/react';
+import * as PXBThemes from '@pxblue/themes/react';
 
 render(
-  <MuiThemeProvider theme={createMuiTheme(EatonThemes.blue)}>
+  <MuiThemeProvider theme={createMuiTheme(PXBThemes.blue)}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
